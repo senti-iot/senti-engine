5a24eceab309049a176c80caa16c4a6ad1e4f384
Christian Broberg <cb@cb-pro.local>
2019-04-15 12:07:46 +0200

    Added travis deployment

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -2,19 +2,19 @@
 # chmod 700 api-restart.sh
 
 if [[ "$1" == "master" ]]; then 
-	npm install --prefix /srv/nodejs/senti/services/gateway/production
-	systemctl restart senti-gateway.service
+	npm install --prefix /srv/nodejs/senti/services/engine/production
+	systemctl restart senti-engine.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway MASTER updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Engine MASTER updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
 
 if [[ "$1" == "dev" ]]; then 
-	npm install --prefix /srv/nodejs/senti/services/gateway/development
-	systemctl restart senti-gateway-dev.service
+	npm install --prefix /srv/nodejs/senti/services/engine/development
+	systemctl restart senti-engine-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Engine DEV updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
